460c5673661f761387b2aec5e59b5142fa8b4769
aaron blum <blumaa@gmail.com>
2025-07-14 22:33:35 +0200

    add claude-solution branch to ci/cd

--- src/components/BaseCard/BaseCard.vue
+++ src/components/BaseCard/BaseCard.vue
@@ -2,31 +2,29 @@
 import { defineComponent } from 'vue';
 
 defineComponent({
-  name: 'BaseCard'
+  name: 'BaseCard',
 });
 
-const props = withDefaults(
-  defineProps<{
-    variant?: 'default' | 'header' | 'custom';
-  }>(),
-  {
-    variant: 'default'
-  }
-);
+interface Props {
+  variant?: 'default' | 'header' | 'custom';
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  variant: 'default',
+});
 
 const cardClasses = {
-  default: 'col-start-5 col-span-4 row-start-3 row-span-9 border border-neutral-300 rounded bg-white py-2 px-4',
+  default:
+    'col-start-5 col-span-4 row-start-3 row-span-9 border border-neutral-300 rounded bg-white py-2 px-4',
   header: 'col-span-full row-start-2 border border-neutral-300 rounded bg-white py-2 px-4',
-  custom: 'border border-neutral-300 rounded bg-white py-2 px-4'
+  custom: 'border border-neutral-300 rounded bg-white py-2 px-4',
 };
 </script>
 
 <template>
-  <div :class="cardClasses[variant]">
+  <div :class="cardClasses[props.variant]">
     <slot />
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
