4deeda4fb511007fb99fb31ab51245bf1f672a17
aaron blum <blumaa@gmail.com>
2025-07-15 15:35:29 +0200

    bug: fix button hover text color

--- src/components/BaseButton/BaseButton.vue
+++ src/components/BaseButton/BaseButton.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <button class="border border-neutral-300 hover:bg-neutral-100 rounded px-2 py-1">
+  <button class="border border-neutral-300 hover:bg-neutral-400 hover:text-white rounded px-2 py-1">
     <slot />
   </button>
 </template>
