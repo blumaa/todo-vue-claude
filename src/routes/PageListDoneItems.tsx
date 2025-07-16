7bc7b58aad749e1135da27fd288455f27fd2b89f
aaron blum <blumaa@gmail.com>
2025-07-16 22:56:00 +0200

    feat: list completed todos

--- src/routes/PageListDoneItems.tsx
+++ src/routes/PageListDoneItems.tsx
@@ -1,9 +1,25 @@
 import { BaseCard } from '../components/BaseCard';
+import { useSelector } from "react-redux";
+import { type RootState } from "../stores";
+import { ListItem } from "../components/ListItem";
+import type { ListItem as ListItemType } from "@/types";
 
 export const PageListDoneItems = () => {
+  const { todos }= useSelector((state: RootState) => state.todoList);
+
   return (
     <BaseCard className="col-start-5 col-span-4 row-start-2">
-      List of Done Items
+      <ul className="flex flex-col gap-3">
+        {todos
+          .filter((todo: ListItemType) => todo.state === "done")
+          .map((todo: ListItemType) => (
+            <ListItem
+              key={todo.id}
+              description={todo.description}
+              state={todo.state}
+            />
+          ))}
+      </ul>
     </BaseCard>
   );
 };
