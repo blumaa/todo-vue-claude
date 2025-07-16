3927b2ac6e3a2c3737e161b21a33e13742bc7e9b
aaron blum <blumaa@gmail.com>
2025-07-16 20:10:07 +0200

    feat: list example todos in pending

--- src/routes/PageListPendingItems.tsx
+++ src/routes/PageListPendingItems.tsx
@@ -1,24 +1,33 @@
-import { useMemo } from 'react';
-import { useSelector } from 'react-redux';
-import { BaseCard } from '../components/BaseCard';
-import { ListItem } from '../components/ListItem';
-import { type RootState } from '../stores';
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { BaseCard } from "../components/BaseCard";
+import { ListItem } from "../components/ListItem";
+import { type RootState } from "../stores";
+import type { ListItem as ListItemType } from "@/types";
 
 export const PageListPendingItems = () => {
-  const list = useSelector((state: RootState) => state.todoList.list);
+  const { todos }= useSelector((state: RootState) => state.todoList);
 
   const hasItems = useMemo(() => {
-    return !!list.length;
-  }, [list.length]);
+    return !!todos.length;
+  }, [todos.length]);
 
   return (
     <BaseCard className="col-start-5 col-span-4 row-start-2 row-span-10">
       {!hasItems && <div>No pending Items</div>}
 
       <ul className="flex flex-col gap-3">
-        <ListItem description="Make the Todo list work" />
-        <ListItem description="Make the ListItem Component" state="done" />
+        {todos
+          .filter((todo: ListItemType) => todo.state === "pending")
+          .map((todo: ListItemType) => (
+            <ListItem
+              key={todo.id}
+              description={todo.description}
+              state={todo.state}
+            />
+          ))}
       </ul>
     </BaseCard>
   );
 };
+
--- src/stores/todoListStore.ts
+++ src/stores/todoListStore.ts
@@ -1,22 +1,27 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { type ListItem } from '../types';
+import { createSlice } from "@reduxjs/toolkit";
+import { type ListItem } from "../types";
 
 interface TodoListState {
-  list: ListItem[];
+  todos: ListItem[];
 }
 
 const initialState: TodoListState = {
-  list: []
+  todos: [
+    { id: "1", description: "todo 1", state: "pending" },
+    { id: "2", description: "todo 2", state: "done" },
+    { id: "3", description: "todo 3", state: "pending" },
+  ],
 };
 
 export const todoListSlice = createSlice({
-  name: 'todoList',
+  name: "todoList",
   initialState,
   reducers: {
     // Add reducers as needed
-  }
+  },
 });
 
 // Export actions when they are added
 // export const { } = todoListSlice.actions;
 export default todoListSlice.reducer;
+
