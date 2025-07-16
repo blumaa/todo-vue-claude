62d229b02fc9df1ed6f63968781a68b7e0be1261
aaron blum <blumaa@gmail.com>
2025-07-16 23:52:53 +0200

    add location to header

--- src/components/TheHeader/TheHeader.tsx
+++ src/components/TheHeader/TheHeader.tsx
@@ -1,10 +1,27 @@
 import { BaseCard } from "../BaseCard";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export const TheHeader = () => {
+  const location = useLocation();
+
+  const getPageName = (pathname: string): string => {
+    switch (pathname) {
+      case "/create":
+        return "Create Todo";
+      case "/list-pending":
+        return "Pending Todos";
+      case "/list-done":
+        return "Completed Todos";
+      case "/":
+        return "Home";
+      default:
+        return "Todo App";
+    }
+  };
+
   return (
     <BaseCard className="col-span-full grid auto-flow-col items-center">
-      <h3>Page Name</h3>
+      <h3>{getPageName(location.pathname)}</h3>
       <div className="grid auto-flow-col gap-2 justify-end">
         <Link to="/create" className="btn">
           Create
@@ -19,4 +36,3 @@ export const TheHeader = () => {
     </BaseCard>
   );
 };
-
