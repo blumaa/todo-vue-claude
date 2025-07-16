9eab774818076287ade7d6e2f43b339962722185
aaron blum <blumaa@gmail.com>
2025-07-16 20:09:43 +0200

    feat: add routing to navigation buttons

--- src/components/TheHeader/TheHeader.tsx
+++ src/components/TheHeader/TheHeader.tsx
@@ -1,14 +1,22 @@
-import { BaseCard } from '../BaseCard';
+import { BaseCard } from "../BaseCard";
+import { Link } from "react-router-dom";
 
 export const TheHeader = () => {
   return (
     <BaseCard className="col-span-full grid auto-flow-col items-center">
       <h3>Page Name</h3>
       <div className="grid auto-flow-col gap-2 justify-end">
-        <button>Create</button>
-        <button>Pending</button>
-        <button>Done</button>
+        <Link to="/create" className="btn">
+          Create
+        </Link>
+        <Link to="/list-pending" className="btn">
+          Pending
+        </Link>
+        <Link to="/list-done" className="btn">
+          Done
+        </Link>
       </div>
     </BaseCard>
   );
 };
+
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,30 +1,42 @@
-import { createBrowserRouter, Navigate } from 'react-router-dom';
-import { PageListPendingItems } from './PageListPendingItems';
-import { PageListDoneItems } from './PageListDoneItems';
-import { PageCreateItem } from './PageCreateItem';
+import { createBrowserRouter, Navigate, Outlet } from "react-router-dom";
+import { TheHeader } from "../components/TheHeader";
+import { PageListPendingItems } from "./PageListPendingItems";
+import { PageListDoneItems } from "./PageListDoneItems";
+import { PageCreateItem } from "./PageCreateItem";
+
+const Layout = () => (
+  <>
+    <TheHeader />
+    <Outlet />
+  </>
+);
 
 export const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Navigate to="/list-pending" replace />,
-  },
-  {
-    path: '/list-pending',
-    element: <PageListPendingItems />,
-  },
-  {
-    path: '/list-done',
-    element: <PageListDoneItems />,
-  },
-  {
-    path: '/create',
-    // TODO: Create page to make new items
-    element: <PageCreateItem />,
-  },
-  {
-    path: '/edit/:id',
-    // TODO: Create page to edit items
-    // component: () => import('./*.vue')
-    element: <div>Edit page not implemented</div>,
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/list-pending" replace />,
+      },
+      {
+        path: "list-pending",
+        element: <PageListPendingItems />,
+      },
+      {
+        path: "list-done",
+        element: <PageListDoneItems />,
+      },
+      {
+        path: "create",
+        element: <PageCreateItem />,
+      },
+      {
+        path: "edit/:id",
+        element: <div>Edit page not implemented</div>,
+      },
+    ],
   },
 ]);
+
